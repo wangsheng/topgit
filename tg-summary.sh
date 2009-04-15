8dfd58da480b96822dc988e575216f8c0f4866c3
Yann Hodique <yann.hodique@gmail.com>
2009-04-15 22:03:14 +0200

    introduce label formatter (used for topic branches in graph mode)

--- tg-summary.sh
+++ tg-summary.sh
@@ -5,6 +5,7 @@
 
 terse=
 driver=default_driver
+label_tpl="%n"
 
 ## Parse options
 
@@ -19,6 +20,9 @@ while [ -n "$1" ]; do
 	    graph=1
             driver=${arg#--graph=}_driver
             ;;
+	"--label="*)
+            label_tpl=${arg#--label=}
+            ;;
 	*)
 	    echo "Usage: tg [...] summary [-t | --graph=<driver>]" >&2
 	    echo '  where <driver> is one of "dot", "txt"' >&2
@@ -31,8 +35,35 @@ curname="$(git symbolic-ref HEAD | sed 's#^refs/\(heads\|top-bases\)/##')"
 ! [ -n "$terse" -a -n "$graph" ] ||
 	die "-t and --graph options are mutual exclusive"
 
+# $current$nonempty$remote$rem_update$deps_update$deps_missing$base_update 
+# $rev 
+# $name"
+get_label() {
+    flags=$1
+    r=$2
+    n=$3
+    N=${n#t/}
+
+    echo $flags | sed 's/\(.\)/\1 /g' | {
+        read c e R U u d b
+        
+        label='"'$label_tpl'"'
+        new_label=""
+        
+        while [ true ]; do
+            new_label=`echo $label | sed 's/%\(\.[0-9]\)\?\([^.s]\)\(.*\)/%\1s\3 \$\2/'`
+            if [ "$new_label" = "$label" ]; then
+                break
+            fi
+            label=$new_label
+        done
+
+        echo `eval "printf $new_label"`
+    }
+}
+
 default_driver() {
-    while read flags rev name; do
+    while read flags rev name label; do
         flags=`echo $flags | tr '.' ' '`
 	if [ "$(git rev-parse "$name")" != "$rev" ]; then
 	    subject="$(git cat-file blob "$name:.topmsg" | sed -n 's/^Subject: //p')"
@@ -64,7 +95,8 @@ graph [
 
 EOT
 
-    while read flags rev name; do
+    while read flags rev name label; do
+        echo "{node [label=\"$label\"] \"$name\";}"
 	git cat-file blob "$name:.topdeps" | while read dep; do
 	    dep_is_tgish=true
 	    ref_exists "refs/top-bases/$dep"  ||
@@ -73,7 +105,7 @@ EOT
 		echo "\"$name\" -> \"$dep\";"
 	    fi
 	done
-    done
+    done | sort # hack: make sure lines starting with a brace come first
 
     echo '}'
 }
@@ -118,8 +150,9 @@ git for-each-ref refs/top-bases |
 		base_update='.'
 		branch_contains "$name" "refs/top-bases/$name" || base_update='B'
 
-		printf '%s %s %s\n' "$current$nonempty$remote$rem_update$deps_update$deps_missing$base_update" \
-			"$rev" "$name"
+                flags="$current$nonempty$remote$rem_update$deps_update$deps_missing$base_update"
+                label=`get_label "$flags" "$rev" "$name"`
+		printf '%s %s %s %s\n' "$flags" "$rev" "$name" "$label"
 	done | $driver
 
 # vim:noet
